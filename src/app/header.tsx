e97e186525cccfb47be7ffb491ea33de7121d937
kazukitash <kazukitash@gmail.com>
2024-06-22 22:46:22 +0900

    logoのhover時にpointerになるように変更

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Header = () => {
   return (
     <header className="flex relative h-10 my-1 md:my-2 justify-center items-center">
-      <Link href="/" className="block w-max h-full">
+      <Link href="/" className="block w-max h-full hover:cursor-pointer">
         <Image src="/logo.png" alt="Yukima Logo" width={108} height={42} />
       </Link>
     </header>
